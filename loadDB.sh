98f15042a3752763ab30ec98de323561e7f506b5
Jordan Lee <jmlee2014@gmail.com>
2019-03-20 15:58:25 -0400

    added shell script to load database

---
+++ loadDB.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+echo "ENTER DATABASE NAME: "
+read dbname
+
+echo "ENTER DATABASE USERNAME: "
+read dbuser
+
+# echo "ENTER DATABASE PASSWORD: "
+# read dbpassword
+
+createdb -h localhost -p 5432 -U $dbuser $dbname
+
+psql -U $dbuser $dbname < "./src/main/resources/ResolutionITS_DB.pqsql"
